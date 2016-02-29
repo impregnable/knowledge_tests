05c9c4eb3b638d52dc3a4ca0f4c57e44e374f404
impregnable <ImpregnableFiend@gmail.com>
2016-03-01 01:49:28 +0300

    Fix & refactoring controller

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -7,14 +7,21 @@
   use Symfony\Component\HttpFoundation\Response;
   use AppBundle\Entity\Test;
   use AppBundle\Entity\Question;
-  use AppBundle\Entity\Answer;
 
   class MainController extends Controller {
 
     /**
     * @Route("/tests")
     */
-    function tests() {
-      return $this->render("/tests.html.twig");
+    function getTests() {
+      // create instance of QueryBuilder
+     $qb = $this->getDoctrine()->getManager()->createQueryBuilder();
+     // create query that get some properties from database
+     $query = $qb->select(array('t.name', 'COUNT(q) as questions_count', 't.id'))
+     ->from('AppBundle\Entity\Test', 't')
+     ->leftJoin('AppBundle\Entity\Question', 'q', \Doctrine\ORM\Query\Expr\Join::WITH, 'q.test = t.id')
+     ->groupBy('t.id');
+     // render template tests
+     return  $this->render("/tests.html.twig", array('tests' => $query->getQuery()->getResult()));
     }
   }
