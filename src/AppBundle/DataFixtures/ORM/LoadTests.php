d1c5615c4c19653ec7b5cc4aa61dc7141fde3422
impregnable <ImpregnableFiend@gmail.com>
2016-03-02 20:02:48 +0300

    Edit model

--- src/AppBundle/DataFixtures/ORM/LoadTests.php
+++ src/AppBundle/DataFixtures/ORM/LoadTests.php
@@ -1,30 +1,65 @@
 <?php
 
-  namespace AppBundle\DataFixtures\ORM;
+namespace AppBundle\DataFixtures\ORM;
 
-  use Doctrine\Common\DataFixtures\FixtureInterface;
-  use Doctrine\Common\Persistence\ObjectManager;
-  use AppBundle\Entity\Test;
-  use AppBundle\Entity\Question;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Test;
+use AppBundle\Entity\Question;
+use AppBundle\Entity\Answer;
 
-  class LoadTests implements FixtureInterface
+class LoadTests implements FixtureInterface
+{
+  public function load(ObjectManager $manager)
   {
-      public function load(ObjectManager $manager)
-      {
-          $testNames = array('Test1', 'Test2', 'Test3');
-            foreach ($testNames as $testName) {
-              $test = new Test();
-              $test->setName($testName);
-              $manager->persist($test);
-              $manager->flush();
+    $testsData = array(
+      array(
+        'name' => 'First test',
+        'questions' => array(
+          array(
+            'text' => 'Select A',
+            'answers' => array(
+              array('text' => 'A', 'isCorrect' => true),
+              array('text' => 'B', 'isCorrect' => false),
+              array('text' => 'C', 'isCorrect' => false)
+            )
+          ),
+          array(
+            'text' => 'Select A and B',
+            'answers' => array(
+              array('text' => 'A', 'isCorrect' => true),
+              array('text' => 'B', 'isCorrect' => true),
+              array('text' => 'C', 'isCorrect' => false)
+            )
+          ),
+          array(
+            'text' => 'Type C',
+            'answers' => array(
+              array('text' => 'C', 'isCorrect' => true)
+            )
+          )
+        )
+      )
+    );
+    foreach ($testsData as $testData) {
+      $test = (new Test())->setName($testData['name']);
+      $manager->persist($test);
+      $manager->flush();
 
-          $questionNames = array('Question1', 'Question2', 'Question3');
-            foreach ($questionNames as $questionName) {
-              $question = new Question();
-              $question->setText($questionName)->setTest($test);
-              $manager->persist($question);
-              $manager->flush();
-            }
-            }
+      foreach ($testData['questions'] as $questionData) {
+        $question = (new Question())->setText($questionData['text'])->setTest($test);
+        $manager->persist($question);
+        $manager->flush();
+
+        foreach($questionData['answers'] as $answerData) {
+          $answer = (new Answer())
+            ->setText($answerData['text'])
+            ->setIsCorrect($answerData['isCorrect'])
+            ->setQuestion($question);
+          $manager->persist($answer);
+          $manager->flush();
+        }
       }
+    }
   }
+}
